27d16a67f8f4ae45867daf114bc89f25bb0f22ec
Thiago <thiagobarros@cjr.org.br>
2019-04-11 21:17:18 -0300

    Criando a pagina de introdução sem o spalsh inicial

--- app/src/main/java/com/example/meau/MainActivity.java
+++ app/src/main/java/com/example/meau/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.meau;
 
+import android.app.Activity;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
         setContentView(R.layout.activity_main);
     }
 }
